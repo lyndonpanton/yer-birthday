0ec45d92a0ed1c95fd78ffffa28d5b979926e893
Lyndon Panton <LyndonPanton@gmail.com>
2023-02-07 21:59:26 +0000

    Tested for valid birthday input

--- Exercise8/Exercise8/Program.cs
+++ Exercise8/Exercise8/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            bool passed = false;
+
+            while (!passed)
+            {
+                Console.Write("Enter the month of your birthday: ");
+                string birthday = Console.ReadLine().ToLower();
+
+                switch (birthday)
+                {
+                    case "january":
+                    case "february":
+                    case "march":
+                    case "april":
+                    case "may":
+                    case "june":
+                    case "july":
+                    case "august":
+                    case "september":
+                    case "october":
+                    case "november":
+                    case "december":
+                        passed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid month entered");
+                        Console.WriteLine("Please try again");
+                        Console.WriteLine();
+                        break;
+                }
+
+            }
         }
     }
 }
